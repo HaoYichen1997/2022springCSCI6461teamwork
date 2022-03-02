c301dd37985796e8ee77bfa810bc23def263fc2a
HYC <hycreator1002@gmail.com>
2022-03-01 20:59:31 -0500

    cache and phase instruction 6

--- pythonProject3/main.py
+++ pythonProject3/main.py
@@ -588,34 +588,34 @@ def run_Single_Step():
         print('opcode is 042')
         show_general(stx042_result)
     elif opcode == 8: #Jump if Zero
-        jp010_result = instr.JZ10(instr.ir.num)
+        jp010_result = instr.jz010(instr.ir.num)
         print('opcode is 010')
         show_general(jp010_result)
-    elif opcode == 11:  # Jump if Zero
-        jne11_result = instr.jne11(instr.ir.num)
+    elif opcode == 9:  # Jump if Zero
+        jne11_result = instr.jne011(instr.ir.num)
         print('opcode is 011')
         show_general(jne11_result)
-    elif opcode == 12:  # Jump if Zero
+    elif opcode == 10:  # Jump if Zero
         jcc012_result = instr.jcc012(instr.ir.num)
         print('opcode is 012')
         show_general(jcc012_result)
-    elif opcode == 13:  # Jump if Zero
+    elif opcode == 11:  # Jump if Zero
         jma013_result = instr.jma013(instr.ir.num)
         print('opcode is 013')
         show_general(jma013_result)
-    elif opcode == 14:  # Jump if Zero
+    elif opcode == 12:  # Jump if Zero
         jsr014_result = instr.jsr014(instr.ir.num)
         print('opcode is 014')
         show_general(jsr014_result)
-    elif opcode == 15:  # Jump if Zero
+    elif opcode == 13:  # Jump if Zero
         rfs015_result = instr.rfs015(instr.ir.num)
         print('opcode is 015')
         show_general(rfs015_result)
-    elif opcode == 16:  # Jump if Zero
+    elif opcode == 14:  # Jump if Zero
         sob016_result = instr.sob016(instr.ir.num)
         print('opcode is 016')
         show_general(sob016_result)
-    elif opcode == 17:  # Jump if Zero
+    elif opcode == 15:  # Jump if Zero
         jge017_result = instr.jge017(instr.ir.num)
         print('opcode is 017')
         show_general(jge017_result)
